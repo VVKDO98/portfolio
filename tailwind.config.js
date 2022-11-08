3816633672b7988486343ae022c3df88a0179ae6
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-11-08 15:51:34 +0100

    fix colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./public/**/*.{html,js}",
-    "./index.html"
-  ],
-  theme: {
-    extend: {
-      fontFamily:{
-        opensans: ["Open sans", "sans-serif"],
-        oswald: ["Oswald", "sans-serif"],
-      }
+    content: ["./public/**/*.{html,js}", "./index.html"],
+    theme: {
+        extend: {
+            fontFamily: {
+                opensans: ["Open sans", "sans-serif"],
+                oswald: ["Oswald", "sans-serif"],
+            },
+        },
+        colors: {
+            blue: "#056CF2",
+            yellow: "#F2A005",
+            black: "#000000",
+            white: "#FFFFFF",
+        },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
